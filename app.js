8f1cfca49535ceec1d061132b91e985aad370919
Megan Buglio <meganbuglio@Megans-MacBook-Pro.local>
2024-02-16 09:15:59 -0600

    adding initial files

---
+++ app.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const app = express();
+const ExpressError = require('./expressError');
+
+const {convertAndValidateNumsArray, findMode, findMean, findMedian} = require('./helpers');
+
+app.get('/mean', function(req,res){
+    if(!req.query.nums){
+        throw new ExpressError('Please pass a query key of numbers with a comma-seperated list of numbers', 400)
+    }
+    let numsAsStrings = req.query.nums.split(',');
+    let nums = convertAndValidateNumsArray(numsAsStrings);
+    if(nums instanceof Error){
+        throw new ExpressError(nums.message)
+    }
+
+    let result = {
+        operation: 'mean',
+        result: findMean(nums)
+    }
+
+    return res.send(result);
+});
+
+app.get('/median', function(req,res){
+    if(!req.query.nums){
+        throw new ExpressError('Please pass a query key of numbers with a comma-seperated list of numbers', 400)
+    }
+    let numsAsStrings = req.query.nums.split(',');
+    let nums = convertAndValidateNumsArray(numsAsStrings);
+    if(nums instanceof Error){
+        throw new ExpressError(nums.message)
+    }
+
+    let result = {
+        operation: 'median',
+        result: findMedian(nums)
+    }
+
+    return res.send(result);
+});
+
+app.get('/mode', function(req,res){
+    if(!req.query.nums){
+        throw new ExpressError('Please pass a query key of numbers with a comma-seperated list of numbers', 400)
+    }
+    let numsAsStrings = req.query.nums.split(',');
+    let nums = convertAndValidateNumsArray(numsAsStrings);
+    if(nums instanceof Error){
+        throw new ExpressError(nums.message)
+    }
+
+    let result = {
+        operation: 'mode',
+        result: findMode(nums)
+    }
+
+    return res.send(result);
+});
+
+app.use(function(req,res,next){
+    const err = new ExpressError("Not Found", 404);
+    return next (err);
+});
+
+app.use(function(err, res, next){
+    res.status(err.status || 500);
+
+    return res.json({
+        error: err,
+        message: err.message
+    })
+});
+
+app.listen(3000, function(){
+    console.log(`Server startingon port 3000`);
+});
